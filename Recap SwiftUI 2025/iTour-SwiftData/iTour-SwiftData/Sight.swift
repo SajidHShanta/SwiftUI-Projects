3b5589a4cb0b16f4fa61e8ff6e1408350949bb16
S. M. Sajid Hasan Shanta <sajidhasan673@gmail.com>
2025-06-26 13:40:39 +0600

    iTour:  working with SwiftData relationships

---
+++ Recap SwiftUI 2025/iTour-SwiftData/iTour-SwiftData/Sight.swift
@@ -0,0 +1,18 @@
+//
+//  Sight.swift
+//  iTour-SwiftData
+//
+//  Created by Sajid Shanta on 26/6/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
